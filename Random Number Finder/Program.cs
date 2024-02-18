190709bb3316f3d8d786f2c71989726d6dd5a809
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-02-19 00:50:26 +0300

    Add files via upload

---
+++ Random Number Finder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Number_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(0,5);
+            int yourNumber;
+            while (true)
+            {
+                Console.Write("Please enter your number: ");
+                yourNumber = int.Parse(Console.ReadLine());
+
+                if (randomNumber == yourNumber)
+                {
+                    Console.WriteLine("Succsessful ! You'r Find Number");
+                    Console.WriteLine("Computer Number:{0}",randomNumber);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("upss! you'r Not Find Number ");
+                }
+            }
+        }
+    }
+}
